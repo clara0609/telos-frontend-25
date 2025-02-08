314839bc74988a93269c4750d76fc72b20fb8d3c
clara0609 <duyen1995dl@gmail.com>
2025-02-08 09:05:54 +0700

    2nd version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,28 @@
 module.exports = {
   content: [
+    "./*/*.html",
     "./*.html"
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    extend: {
+      colors: {
+        abc: {
+          500: "#db03f3",
+          600: "#db03f3",
+        },
+        red: {
+          400: "db03f3"
+        }
+      },
+      spacing : {
+        1: "4px"
+      },
+      fontSize: {
+        h2: ["48px","64px"],
+        lead1: ["18px","32px"]
+      }
+    },
   },
   plugins: [],
 }
